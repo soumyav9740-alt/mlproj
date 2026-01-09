5d82a49eef09effd76b7a4ebd68c315623a11e17
soumya <soumyav9740@gmail.com>
2026-01-09 13:50:00 +0530

    added components

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,10 @@ from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -51,5 +55,10 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion(data_source='/Users/soumyaverma/Documents/MLproj/notebook/data/stud.csv')
+    train_data, test_data=obj.initiate_data_ingestion(data_source='notebook/data/stud.csv')
 
+    data_transformation = DataTransformation()
+    train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
---
+++ src/utils.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import dill
+from sklearn.metrics import r2_score
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model_name = list(models.keys())[i]
+
+            # Train the model
+            model.fit(X_train, y_train)
+
+            #predict on training data
+            y_train_pred = model.predict(X_train)
+
+            # Predict on test data
+            y_test_pred = model.predict(X_test)
+
+            #calculate r2 score for train and test data
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_model_score
+
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
